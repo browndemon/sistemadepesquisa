b3125a0279201c5562c26df4c11835fcf99d75b4
Lucas Antonio <lucas@holdercloud.com>
2018-07-15 00:06:28 -0300

    Estrutura MVC e inicio do template

---
+++ template/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Sistema de pesquisa</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+<div class="header">
+	<div class="logo">
+		<img src="assets/img/logo.png" alt="sistema de pesquisa">
+	</div>
+</div>
+<div class="componentes">
+	<form method="post">
+		<label><img src="assets/img/processador.png" alt="Processadores"></label>
+		<select>
+			<option>Processador 1</option>
+			<option>Processador 2</option>
+			<option>Processador 3</option>
+		</select><br/>
+		<label><img src="assets/img/placamae.png" alt="Placa Mãe"></label>
+		<select>
+			<option>Placa Mãe 1</option>
+			<option>Placa Mãe 2</option>
+			<option>Placa Mãe 3</option>
+		</select><br/>
+		<label><img src="assets/img/water.png" alt="Water Cooler"></label>
+		<select>
+			<option>Water Cooler 1</option>
+			<option>Water Cooler 2</option>
+			<option>Water Cooler 3</option>
+		</select><br/>
+		<label><img src="assets/img/fan.png" alt="Fans"></label>
+		<select>
+			<option>Fan 1</option>
+			<option>Fan </option>
+			<option>Fan 12</option>
+		</select><br/>
+		<label><img src="assets/img/memoria.png" alt="Memória Ram"></label>
+		<select>
+			<option>Memória Ram 1</option>
+			<option>Memória Ram 2</option>
+			<option>Memória Ram 3</option>
+		</select><br/>
+		<label><img src="assets/img/placadevideo.png" alt="Placa de Vídeo"></label>
+		<select>
+			<option>Placa de Vídeo 1</option>
+			<option>Placa de Vídeo 2</option>
+			<option>Placa de Vídeo 3</option>
+		</select><br/>
+		<label><img src="assets/img/hd.png" alt="Hard Disk"></label>
+		<select>
+			<option>Hard Disk 1</option>
+			<option>Hard Disk 2</option>
+			<option>Hard Disk 3</option>
+		</select><br/>
+		<label><img src="assets/img/ssd.png" alt="SSD"></label>
+		<select>
+			<option>SSD 1</option>
+			<option>SSD 2</option>
+			<option>SSD 3</option>
+		</select><br/>
+		<label><img src="assets/img/gravador.png" alt="Gravador"></label>
+		<select>
+			<option>Gravador 1</option>
+			<option>Gravador 2</option>
+			<option>Gravador 3</option>
+		</select><br/>
+		<label><img src="assets/img/fonte.png" alt="Fonte"></label>
+		<select>
+			<option>Fonte 1</option>
+			<option>Fonte 2</option>
+			<option>Fonte 3</option>
+		</select><br/>
+		<label><img src="assets/img/gabinete.png" alt="Gabinete"></label>
+		<select>
+			<option>Gabinete 1</option>
+			<option>Gabinete 2</option>
+			<option>Gabinete 3</option>
+		</select><br/>
+		<label><img src="assets/img/monitor.png" alt="Monitor"></label>
+		<select>
+			<option>Monitor 1</option>
+			<option>Monitor 2</option>
+			<option>Monitor 3</option>
+		</select><br/>
+	</form>
+</div>
+</body>
+</html>
