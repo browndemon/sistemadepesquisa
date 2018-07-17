cde0965bd66aeac99d2d451bbfa30a2cbed498a3
Matheus Henrique <mhqsoftware@gmail.com>
2018-07-17 18:42:01 -0300

    Iniciando Front-end

--- template/index.php
+++ template/index.php
@@ -3,89 +3,241 @@
 <head>
 	<meta charset="utf-8">
 	<title>Sistema de pesquisa</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    
+    <!-- VIEWPORT -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+	
+    <!-- RESET -->
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+    
+    <!-- CSS ALL -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    
+    <!-- FONTES LETRA -->
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans|Raleway" rel="stylesheet">
+    
 </head>
 <body>
-<div class="header">
-	<div class="logo">
-		<img src="assets/img/logo.png" alt="sistema de pesquisa">
-	</div>
-</div>
-<div class="componentes">
+
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img class="logo-desktop" src="assets/img/logo/banner-desktop.png" alt="sistema de pesquisa">
+            </div>
+        </div>
+    </header>
+
+    
+<main>  
+    
+<section id="banner">
+    <img class="banner-desktop" src="assets/img/banner/banner01.jpg" alt=""/>
+</section>
+    
+<div class="montepc">
+    <div class="bgimgmp">
+        <div class="bgmpc container">
+            <h2>Monte seu computador mais barato.</h2>
+            <p>Pesquise peças agora !</p>
+        </div>
+    </div>
+</div>    
+    
+<div class="componentes container">
 	<form method="post">
-		<label><img src="assets/img/processador.png" alt="Processadores"></label>
-		<select>
-			<option>Processador 1</option>
-			<option>Processador 2</option>
-			<option>Processador 3</option>
-		</select><br/>
-		<label><img src="assets/img/placamae.png" alt="Placa Mãe"></label>
-		<select>
-			<option>Placa Mãe 1</option>
-			<option>Placa Mãe 2</option>
-			<option>Placa Mãe 3</option>
-		</select><br/>
-		<label><img src="assets/img/water.png" alt="Water Cooler"></label>
-		<select>
-			<option>Water Cooler 1</option>
-			<option>Water Cooler 2</option>
-			<option>Water Cooler 3</option>
-		</select><br/>
-		<label><img src="assets/img/fan.png" alt="Fans"></label>
-		<select>
-			<option>Fan 1</option>
-			<option>Fan </option>
-			<option>Fan 12</option>
-		</select><br/>
-		<label><img src="assets/img/memoria.png" alt="Memória Ram"></label>
-		<select>
-			<option>Memória Ram 1</option>
-			<option>Memória Ram 2</option>
-			<option>Memória Ram 3</option>
-		</select><br/>
-		<label><img src="assets/img/placadevideo.png" alt="Placa de Vídeo"></label>
-		<select>
-			<option>Placa de Vídeo 1</option>
-			<option>Placa de Vídeo 2</option>
-			<option>Placa de Vídeo 3</option>
-		</select><br/>
-		<label><img src="assets/img/hd.png" alt="Hard Disk"></label>
-		<select>
-			<option>Hard Disk 1</option>
-			<option>Hard Disk 2</option>
-			<option>Hard Disk 3</option>
-		</select><br/>
-		<label><img src="assets/img/ssd.png" alt="SSD"></label>
-		<select>
-			<option>SSD 1</option>
-			<option>SSD 2</option>
-			<option>SSD 3</option>
-		</select><br/>
-		<label><img src="assets/img/gravador.png" alt="Gravador"></label>
-		<select>
-			<option>Gravador 1</option>
-			<option>Gravador 2</option>
-			<option>Gravador 3</option>
-		</select><br/>
-		<label><img src="assets/img/fonte.png" alt="Fonte"></label>
-		<select>
-			<option>Fonte 1</option>
-			<option>Fonte 2</option>
-			<option>Fonte 3</option>
-		</select><br/>
-		<label><img src="assets/img/gabinete.png" alt="Gabinete"></label>
-		<select>
-			<option>Gabinete 1</option>
-			<option>Gabinete 2</option>
-			<option>Gabinete 3</option>
-		</select><br/>
-		<label><img src="assets/img/monitor.png" alt="Monitor"></label>
-		<select>
-			<option>Monitor 1</option>
-			<option>Monitor 2</option>
-			<option>Monitor 3</option>
-		</select><br/>
+		<div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/processador.png" alt="Processadores">
+                        <h3>Processador</h3>
+                    </div>   
+                    <select>
+                        <option>Processador 1</option>
+                        <option>Processador 2</option>
+                        <option>Processador 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/placa-mae.png" alt="Placa Mãe">
+                        <h3>Placa Mãe</h3>
+                    </div>   
+                    <select>
+                        <option>Placa Mãe 1</option>
+                        <option>Placa Mãe 2</option>
+                        <option>Placa Mãe 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>      
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/fan.png" alt="Water Cooler">
+                        <h3>Water Cooler</h3>
+                    </div>   
+                    <select>
+                        <option>Water Cooler 1</option>
+                        <option>Water Cooler 2</option>
+                        <option>Water Cooler 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>   
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/fan.png" alt="Fans">
+                        <h3>Fan</h3>
+                    </div>   
+                    <select>
+                        <option>Fan 1</option>
+                        <option>Fan 2</option>
+                        <option>Fan 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>   
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/ram.png" alt="Memória Ram">
+                        <h3>Memória Ram</h3>
+                    </div>   
+                    <select>
+                        <option>Memória Ram 1</option>
+                        <option>Memória Ram 2</option>
+                        <option>Memória Ram 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>   
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/placa-mae.png" alt="Placa de Vídeo">
+                        <h3>Placa de Vídeo</h3>
+                    </div>   
+                    <select>
+                        <option>Placa de Vídeo 1</option>
+                        <option>Placa de Vídeo 2</option>
+                        <option>Placa de Vídeo 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>   
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/placa-mae.png" alt="Hard Disk">
+                        <h3>Hard Disk</h3>
+                    </div>   
+                    <select>
+                        <option>Hard Disk 1</option>
+                        <option>Hard Disk 2</option>
+                        <option>Hard Disk 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>   
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/placa-mae.png" alt="SSD">
+                        <h3>SSD</h3>
+                    </div>   
+                    <select>
+                        <option>SSD 1</option>
+                        <option>SSD 2</option>
+                        <option>SSD 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>   
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/placa-mae.png" alt="Gravador">
+                        <h3>Gravador</h3>
+                    </div>   
+                    <select>
+                        <option>Gravador 1</option>
+                        <option>Gravador 2</option>
+                        <option>Gravador 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>   
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/placa-mae.png" alt="Fonte">
+                        <h3>Fonte</h3>
+                    </div>   
+                    <select>
+                        <option>Fonte 1</option>
+                        <option>Fonte 2</option>
+                        <option>Fonte 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div> 
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/placa-mae.png" alt="Gabinete">
+                        <h3>Gabinete</h3>
+                    </div>   
+                    <select>
+                        <option>Gabinete 1</option>
+                        <option>Gabinete 2</option>
+                        <option>Gabinete 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div> 
+        
+        <div class="divcomp">
+            <div class="bgcomp">
+                <div class="bgcompcor">
+                    <div class="imagem-nome">
+                        <img src="assets/img/componentes/placa-mae.png" alt="Monitor">
+                        <h3>Monitor</h3>
+                    </div>   
+                    <select>
+                        <option>Monitor 1</option>
+                        <option>Monitor 2</option>
+                        <option>Monitor 3</option>
+                    </select>
+                </div>    
+            </div> 
+        </div>   
 	</form>
 </div>
+</main>      
 </body>
 </html>
